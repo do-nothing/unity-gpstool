30e175e2e8ecbee99427a8ac6ebf4a1e5f02186e
do-nothing <lj110119@aliyun.com>
2017-04-13 10:47:44 +0800

    add MapController.cs

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapController : MonoBehaviour {
 
-	// Use this for initialization
+    private Material material;
 	void Start () {
-		
+        material = GetComponent<Image>().material;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        if (Input.touchCount == 1) {
+            Vector2 offset = material.GetTextureOffset("_MainTex");
+            offset.x += 0.1f;
+            material.SetTextureOffset("_MainTex", offset);
+        }
 	}
 }
